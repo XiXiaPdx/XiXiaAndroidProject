e4daca1f557b67e3a4c0b4ca4e42767b35dc1a48
Xi Xia <triplextension@gmail.com>
2017-05-28 22:31:51 -0700

    picture IDs are displaying in ListView

--- app/src/main/java/com/blueoxgym/xixiaandroidproject/MainActivity.java
+++ app/src/main/java/com/blueoxgym/xixiaandroidproject/MainActivity.java
@@ -2,7 +2,8 @@ package com.blueoxgym.xixiaandroidproject;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import com.blueoxgym.xixiaandroidproject.Models.Picture;
 import com.blueoxgym.xixiaandroidproject.Services.UnSplashService;
@@ -13,17 +14,21 @@ import org.json.JSONException;
 import java.io.IOException;
 import java.util.ArrayList;
 
+import butterknife.Bind;
+import butterknife.ButterKnife;
 import okhttp3.Call;
 import okhttp3.Callback;
 import okhttp3.Response;
 
 public class MainActivity extends AppCompatActivity {
     public ArrayList<Picture> mPictures = new ArrayList<>();
-
+    @Bind(R.id.pictureListView)
+    ListView mPictureListView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
         getFoodPictures();
     }
     public void getFoodPictures(){
@@ -40,9 +45,17 @@ public class MainActivity extends AppCompatActivity {
                     String jsonData = response.body().string();
                     JSONArray picJSONArray = new JSONArray(jsonData);
                      mPictures = unSplashService.processResults(picJSONArray);
-                    for(Picture picture: mPictures){
-                        Log.d("Pic URL ", picture.getImageUrl());
-                    }
+                    MainActivity.this.runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            String[] pictureIDs = new String[mPictures.size()];
+                            for (int i = 0; i < pictureIDs.length; i++) {
+                                pictureIDs[i] = mPictures.get(i).getID();
+                            }
+                            ArrayAdapter adapter = new ArrayAdapter(MainActivity.this, android.R.layout.simple_list_item_1, pictureIDs);
+                            mPictureListView.setAdapter(adapter);
+                        }
+                    });
                 } catch (IOException e) {
                     e.printStackTrace();
                 } catch (JSONException e){
