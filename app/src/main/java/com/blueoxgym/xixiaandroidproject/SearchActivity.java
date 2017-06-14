ec4f91edd6835a0b90d8d074da9f848f76c9d09f
Xi Xia <triplextension@gmail.com>
2017-06-14 11:40:08 -0700

    yelp API for location works. Returning

--- app/src/main/java/com/blueoxgym/xixiaandroidproject/SearchActivity.java
+++ app/src/main/java/com/blueoxgym/xixiaandroidproject/SearchActivity.java
@@ -5,15 +5,29 @@ import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.widget.TextView;
+
+import com.blueoxgym.xixiaandroidproject.Models.Restaurant;
+import com.blueoxgym.xixiaandroidproject.Services.YelpService;
+
+import java.io.IOException;
+import java.util.ArrayList;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.Response;
 
 public class SearchActivity extends AppCompatActivity {
-@Bind(R.id.textView3)
-TextView mDisplayPosition;
+    @Bind(R.id.textView3)
+    TextView mDisplayPosition;
+    public String location = "97232";
     private SharedPreferences mSharedPreferences;
+    public ArrayList<Restaurant> mRestaurants;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,7 +35,50 @@ TextView mDisplayPosition;
         setContentView(R.layout.activity_restaurants);
         ButterKnife.bind(this);
         mSharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
+        mDisplayPosition.setText("You want to search for " + mSharedPreferences.getString(Constants.LAST_FOOD_SEARCH, null));
+        getRestaurants(location);
+    }
 
-        mDisplayPosition.setText("You want to search for "+ mSharedPreferences.getString(Constants.LAST_FOOD_SEARCH, null));
+
+    public void getRestaurants(String location) {
+        final YelpService yelpService = new YelpService();
+
+        yelpService.findRestaurants(location, new Callback() {
+
+            @Override
+            public void onFailure(Call call, IOException e) {
+                e.printStackTrace();
+            }
+
+            @Override
+            public void onResponse(Call call, Response response) {
+                mRestaurants = yelpService.processResults(response);
+                Log.d("Restaurants", mRestaurants.get(0).getName());
+
+//                this.runOnUiThread(new Runnable() {
+//                    // Line above states 'getActivity()' instead of previous 'RestaurantListActivity.this'
+//                    // because fragments do not have own context, and must inherit from corresponding activity.
+//
+//
+//                    @Override
+//                    public void run() {
+//                        mAdapter = new RestaurantListAdapter(getActivity(), mRestaurants);
+//                        // Line above states `getActivity()` instead of previous
+//                        // 'getApplicationContext()' because fragments do not have own context,
+//                        // must instead inherit it from corresponding activity.
+//
+//                        mRecyclerView.setAdapter(mAdapter);
+//                        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(getActivity());
+//                        // Line above states 'new LinearLayoutManager(getActivity());' instead of previous
+//                        // 'new LinearLayoutManager(RestaurantListActivity.this);' when method resided
+//                        // in RestaurantListActivity because Fragments do not have context
+//                        // and must instead inherit from corresponding activity.
+//
+//                        mRecyclerView.setLayoutManager(layoutManager);
+//                        mRecyclerView.setHasFixedSize(true);
+//                    }
+//                });
+            }
+        });
     }
 }
