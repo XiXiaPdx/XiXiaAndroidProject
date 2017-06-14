990d8700d1765cfff9814e93dfa3b3dbf7f42c08
Xi Xia <triplextension@gmail.com>
2017-06-14 11:18:12 -0700

    yelp constants and properties added

--- app/src/main/java/com/blueoxgym/xixiaandroidproject/Constants.java
+++ app/src/main/java/com/blueoxgym/xixiaandroidproject/Constants.java
@@ -12,4 +12,13 @@ public class Constants {
     public static final String FIREBASE_CHILD_DESCRIBED_FOODS = "describedFoods" ;
     public static final String LAST_FOOD_SEARCH = "search";
 
+    //Yelp
+    public static final String YELP_CONSUMER_KEY = BuildConfig.YELP_CONSUMER_KEY;
+    public static final String YELP_CONSUMER_SECRET = BuildConfig.YELP_CONSUMER_SECRET;
+    public static final String YELP_TOKEN = BuildConfig.YELP_TOKEN;
+    public static final String YELP_TOKEN_SECRET = BuildConfig.YELP_TOKEN_SECRET;
+    public static final String YELP_BASE_URL = "https://api.yelp.com/v2/search?term=food";
+    public static final String YELP_LOCATION_QUERY_PARAMETER = "location";
+    public static final String PREFERENCES_LOCATION_KEY = "location";
+
 }
